f21c5d606ef32f906b6bb47f9a8c4e1325cda1ca
TheCloudkicker <ckcameron1@gmail.com>
2021-08-10 03:09:41 -0400

    added material ui and base template

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,65 @@
 import React from 'react'
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Link from '@material-ui/core/Link';
+
+const useStyles = makeStyles((theme) => ({
+    icon: {
+        marginRight: theme.spacing(2),
+    },
+    heroContent: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6),
+    },
+    heroButtons: {
+        marginTop: theme.spacing(4),
+    },
+    cardGrid: {
+        paddingTop: theme.spacing(8),
+        paddingBottom: theme.spacing(8),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    cardMedia: {
+        paddingTop: '56.25%', // 16:9
+    },
+    cardContent: {
+        flexGrow: 1,
+    },
+    footer: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(6),
+    },
+}));
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                theCloudkicker
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
 
 function Footer() {
+
+    const classes = useStyles();
+
     return (
-        <div>
-            Footer
-        </div>
+        <footer className={classes.footer}>
+            <Typography variant="h6" align="center" gutterBottom>
+            </Typography>
+            <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
+            </Typography>
+            <Copyright />
+        </footer>
     )
 }
 
